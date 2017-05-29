85b4599e54bf5feb646ac9d51691ef0009a7c9b9
joshua21w <joshua21w@gmail.com>
2017-05-29 12:56:30 +0100

    Updated helpers.py to include a Unicode conversion function, str_to_unicode().

---
+++ rango/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from rango.models import Category, Page
+from rango.models import UserProfile
+
+# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    # Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+        prepopulated_fields = {'slug': ('name',)}
+
+    # Update the registeration to include this customised interface
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+
+
